cd08a33970bcfa2f6dd013fc5eecf64c6d8acae0
davidpampam <72334409+davidpampam@users.noreply.github.com>
2021-04-06 18:30:32 +0700

    Add files via upload

---
+++ pertemuan5/Praktek/MotorVehicle.java
@@ -0,0 +1,18 @@
+public class MotorVehicle extends Vehicle {
+    public int sizeofEngine = 1;
+    public String licencePlate = "H 1 IDN";
+
+    public MotorVehicle() {
+
+    }
+    public MotorVehicle(int sizeEng, String lcPlate) {
+        this.sizeofEngine = sizeEng;
+        this.licencePlate =lcPlate;
+    }
+    public int getSizeofEngine() {
+        return this.sizeofEngine;
+    }
+    public String getLicencePlate() {
+        return this.licencePlate;
+    }
+}
