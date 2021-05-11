104d9b73ef93ebf69fc21e8af8eb9aaea5357230
davidpampam <72334409+davidpampam@users.noreply.github.com>
2021-05-11 20:52:36 +0700

    Delete Praktik8_Kasus2 directory

--- Praktik8_Kasus2/Minibus.java
+++
@@ -1,7 +0,0 @@
-public class Minibus extends Mobil {
-  public Minibus(String mesin, String merk, String model, int harga){
-    super(mesin, merk, model, harga);
-    this.setType("Minibus");
-
-  }
-}
--- Praktik8_Kasus2/Sedan.java
+++
@@ -1,6 +0,0 @@
-public class Sedan extends Mobil {
-  public Sedan(String mesin, String merk, String model, int harga){
-    super(mesin, merk, model, harga);
-    this.setType("Sedan");
-  }
-}
